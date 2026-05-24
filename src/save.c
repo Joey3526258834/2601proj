5281a41a0836201895df62763075cb4878f3aaa8
Joey3526258834 <3526258834@qq.com>
2026-05-24 20:17:20 +0800

    20260524 20:17 修改数据库建表SQL语句，减少冗杂列

--- src/save.c
+++ src/save.c
@@ -88,7 +88,7 @@ void save_sqlite3(Que_Data_t data)
     isaday(sec_num);
   
     char sql_cmd1[512]= {0};
-    sprintf(sql_cmd1,"create table if not exists demo_day%d(id INT, temp REAL, hum REAL,light REAL,smoke INT,time TEXT,sendname TEXT,recvname TEXT);",num_day);//table name  info
+    sprintf(sql_cmd1,"create table if not exists demo_day%d(id INT, temp REAL, hum REAL, light REAL, smoke REAL, time TEXT);",num_day);//table name  info
 
     ret = sqlite3_exec(db,sql_cmd1,NULL,NULL,NULL);
 
@@ -96,7 +96,7 @@ void save_sqlite3(Que_Data_t data)
     sprintf(tim_buf,"%d-%d-%d %d:%d:%d",data.tim.tm_year + 1900,data.tim.tm_mon + 1,data.tim.tm_mday,data.tim.tm_hour,data.tim.tm_min,data.tim.tm_sec);
     //printf("time:%s\n",tim_buf);
     char sql_cmd[512]= {0};
-    sprintf(sql_cmd,"insert into demo_day%d values(%d,%f,%f,%f,%f,'%s','%s','%s');",num_day,data.id,data.temp,data.hum,data.light,data.smoke,tim_buf,data.sendname,data.recvname);//table name  info
+    sprintf(sql_cmd,"insert into demo_day%d values(%d,%f,%f,%f,%f,'%s');",num_day,data.id,data.temp,data.hum,data.light,data.smoke,tim_buf);//table name  info
     char * errmsg;
     ret = sqlite3_exec(db,sql_cmd,NULL,NULL,&errmsg);
     if(SQLITE_OK != ret)
